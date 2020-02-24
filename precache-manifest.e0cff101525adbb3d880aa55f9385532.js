60444ad3c652c1d7184978be889a5db052cd8b7a
Craig <craigoda@gmail.com>
2020-02-23 21:40:23 -0800

    Updates

---
+++ precache-manifest.e0cff101525adbb3d880aa55f9385532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738e33924acc84c05b0bb1f02b278d11",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d66cec72313a4c90c94",
+    "url": "/monsters-rolodex/static/css/main.96907b17.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d66cec72313a4c90c94",
+    "url": "/monsters-rolodex/static/js/main.9410f091.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
